3ba2c602167aedc7fff0fa4cd3df0b1ee84d8713
Yoshitaka Kido <yoshitaka.kido@gmail.com>
2016-12-09 14:58:57 +0900

    Add files via upload

--- mruby/ex1/ex1.rb
+++ mruby/ex1/ex1.rb
@@ -15,21 +15,19 @@ def onTouch1(param1, param2)
         gr_digitalWrite($PIN_LED0, param1)
         a = 0
     end
-    if a == 0 then
-        gr_digitalWrite($PIN_LED3, 1)
-    end 
     Timer(a)
 end
 
 def Timer(a)
     sec = 3
     if a == 0 then
-        While sec > 0 do
+        while sec > 0 do
             gr_digitalWrite($PIN_LED3, 1)
             sec -= 1
         end
     end
     param1 = 0
+    gr_digitalWrite($PON_LED3, param1)
 end
 
 def loop()
