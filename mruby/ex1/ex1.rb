a85c374501ab7607cd04c7e25ed33881702b96ef
Yoshitaka Kido <yoshitaka.kido@gmail.com>
2016-12-09 15:30:35 +0900

    Add files via upload

--- mruby/ex1/ex1.rb
+++ mruby/ex1/ex1.rb
@@ -1,35 +1,44 @@
-def setup()
-    $button1 = GNButton.new(10, 10, "onTouch1")
-
-    gr_pinMode($PIN_LED0, $OUTPUT)
-    gr_pinMode($PIN_LED3, $OUTPUT)
-end
-
-def onTouch1(param1, param2)
-    a = 2
-    if param1 == 1 then
-        gr_digitalWrite($PIN_LED0, param1)
-        a = 1
+class Light
+    def initialize(a)
+        @a = 2
     end
-    if param1 == 0 then
-        gr_digitalWrite($PIN_LED0, param1)
-        a = 0
-    end
-    Timer(a)
-end
 
-def Timer(a)
-    sec = 2
-    if a == 0 then
-        while sec > 0 do
-            gr_digitalWrite($PIN_LED3, 1)
-            sec -= 1
+    def setup()
+        $button1 = GNButton.new(10, 10, "onTouch1")
+
+        gr_pinMode($PIN_LED0, $OUTPUT)
+        gr_pinMode($PIN_LED3, $OUTPUT)
+    end
+
+    def onTouch1(param1, param2)
+        if param1 == 1 then
+            gr_digitalWrite($PIN_LED0, param1)
+            a = 1
         end
-        while sec <= 0 do
-            gr_digitalWrite($PON_LED3, 0)
+        if param1 == 0 then
+            gr_digitalWrite($PIN_LED0, param1)
+            a = 0
         end
+        Timer(a)
+    end
+
+    def Timer(a)
+        sec = 2
+        if a == 0 then
+            while sec > 0 do
+                gr_digitalWrite($PIN_LED3, 1)
+                sec -= 1
+            end
+            while sec <= 0 do
+                gr_digitalWrite($PIN_LED3, 0)
+            end
+        end
+    end
+
+    def loop()
     end
 end
 
-def loop()
-end
+sample = Light.new()
+sample.setup()
+sample.onTouch1(param1, param2)
